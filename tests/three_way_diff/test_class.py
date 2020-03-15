5b5cee8c44f59af7ecc4493e8c893cc67e1f15c5
Oscar Palafox Verna <2333493p@student.gla.ac.uk>
2020-03-15 17:16:37 +0000

    Solves #50 by making three way diff use strings, updated tests and levenshtein file

--- tests/three_way_diff/test_class.py
+++ tests/three_way_diff/test_class.py
@@ -1,13 +1,13 @@
 class TestClass():
-    TEST_STRING_BASE = 'script'
-    TEST_STRING_REPLACE = 'scripf'
-    TEST_STRING_INSERT = 'scrip'
-    TEST_STRING_DELETE = 'scripte'
-    TEST_LIST_BASE = ['this', 'is', 'a', 'test', 'list', 'string']
-    TEST_LIST_REPLACE = ['this', 'is', 'a', 'test', 'list', 'false']
-    TEST_LIST_INSERT = ['this', 'is', 'a', 'test', 'list']
-    TEST_LIST_DELETE = ['this', 'is', 'a', 'test', 'list', 'string', 'extra']
-    TEST_BASE_LEN = len(TEST_STRING_BASE)
+    TEST_STRING_BASE = 'this is a test list string'
+    TEST_STRING_REPLACE = 'this is a test list false'
+    _TEST_STRING_INSERT = 'this is a test list'
+    _TEST_STRING_DELETE = 'this is a test list string extra'
+    TEST_LIST_BASE = TEST_STRING_BASE.split()
+    TEST_LIST_REPLACE = TEST_STRING_REPLACE.split()
+    TEST_LIST_INSERT = _TEST_STRING_INSERT.split()
+    TEST_LIST_DELETE = _TEST_STRING_DELETE.split()
+    TEST_BASE_LEN = len(TEST_LIST_BASE)
     TEST_OPERATION_LIST_EQUAL = [
         ('nothing', index, index) for index in range(TEST_BASE_LEN)]
     TEST_OPERATION_LIST_REPLACE = TEST_OPERATION_LIST_EQUAL[:-1] + \
